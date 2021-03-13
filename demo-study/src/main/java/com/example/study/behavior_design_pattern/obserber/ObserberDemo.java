1b3d80aaf1f3de42579c4c629efd2971df3e6201
HSJ <hesuijin@qq.com>
2021-03-13 14:46:58 +0800

    设计模式 订阅者初始demo

--- demo-study/src/main/java/com/example/study/behavior_design_pattern/obserber/ObserberDemo.java
+++ demo-study/src/main/java/com/example/study/behavior_design_pattern/obserber/ObserberDemo.java
@@ -31,14 +31,9 @@ public class ObserberDemo {
     }
 
     private static void test2() {
-//        1：设置 发布者订阅者   模块方法  Observer Subject
-//        2：创建 发布者订阅者   实现类  ConcreteSubject  ConcreteObserver
-//        3：订阅者 注册到发布者上 表示监听发布者
-//        4：给发布者发送消息  然后监听该发布者的所有订阅者 都能收到
-
         ConcreteSubject subject = new ConcreteSubject();
 
-        List<Observer>  observers = new ArrayList<>();
+        List<Observer> observers = new ArrayList<>();
         observers.add(new ConcreteObserverOne());
         observers.add(new ConcreteObserverTwo());
         subject.registerAllObserver(observers);
